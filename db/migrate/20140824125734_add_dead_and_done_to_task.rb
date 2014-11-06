19fcecaf89e73a65ce86635086a130007c68faa6
marchelio <marchelio479@gmail.com>
2014-11-06 20:09:58 +0200

    pushing heroku2

--- db/migrate/20140824125734_add_dead_and_done_to_task.rb
+++ db/migrate/20140824125734_add_dead_and_done_to_task.rb
@@ -1,6 +1,6 @@
 class AddDeadAndDoneToTask < ActiveRecord::Migration
   def change
     add_column :tasks, :done, :boolean, default: false
-    add_column :tasks, :deadline, :datatime
+    add_column :tasks, :deadline, :datetime
   end
 end
